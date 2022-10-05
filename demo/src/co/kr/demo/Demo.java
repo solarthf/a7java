581158440c39b9554eb04b1b01c83baefdd04f92
solarthf <thfsola@gmail.com>
2022-10-05 14:02:22 +0900

    demo 프로젝트 추가

---
+++ demo/src/co/kr/demo/Demo.java
@@ -0,0 +1,11 @@
+package co.kr.demo;
+//co 패키지 안에 kr패키지 안에 demo패키지
+//.은 경로 구분 = \ 역슬래시랑 같음
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		
+	}
+
+}
